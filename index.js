16e00b6b747aa9392c02ca7ab423283019f7412d
hsoni00123 <38160771+hsoni00123@users.noreply.github.com>
2022-08-09 08:14:44 +0530

    sdw

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ client.on("messageCreate", (message) => {
   request.setRequestHeader('Content-type', 'application/json');
   const params = {
     username: "NewMoon DarkWeb",
-    avatar_url: "https://cdn.discordapp.com/attachments/998675140042817567/1006267341484593352/newmoon.png",
+    avatar_url: "https://cdn.discordapp.com/attachments/998675140042817567/1006392481748889712/unnamed.png",
     title: "??? BY COPY-HOOK",
     content:  "Message sent By" + "-" + "<@!" + message.author.id + ">" + " "+ "\n" + "Msg =" + " "+ message.content 
     
